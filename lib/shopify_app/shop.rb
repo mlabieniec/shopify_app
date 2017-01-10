7ecc0493b981f5b54d884534a6549980de43be92
mlab <michaellabieniec@gmail.com>
2017-01-10 13:24:24 -0500

    update unique

--- lib/shopify_app/shop.rb
+++ lib/shopify_app/shop.rb
@@ -5,7 +5,6 @@ module ShopifyApp
     included do
       validates :shopify_domain, presence: true
       validates :shopify_token, presence: true
-      validates_uniqueness_of :shopify_domain
     end
 
     def with_shopify_session(&block)
